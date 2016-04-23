5943556c728e2297d4ec5ab12fbb7fe50a36de1d
Florent Lamiraux <florent@laas.fr>
2016-04-23 19:13:50 +0200

    Update python script.

--- src/test.py
+++ src/test.py
@@ -1,18 +1,19 @@
 from dynamic_graph.sot.hpp import PathSampler
 ps = PathSampler ('ps')
-ps.loadRobotModel ('hpp_tutorial', 'planar', 'pr2')
-q_init = 47*[0]
-q_init [3] = 1
+ps.loadRobotModel ('sot_youbot', 'planar', 'pr2_sot')
+q_init = 36*[0]
+q_init [0] = 1
+q_init [1] = 1
+q_init [2] = 1
 q_end = q_init [::]
-q_end [0] = 1
+q_end [34] = 2
 ps.addWaypoint (tuple (q_init))
 ps.addWaypoint (tuple (q_end))
-
 ps.configuration.recompute (0)
-ps.setTimeStep (.01)
+ps.setTimeStep (0.1)
 ps.start ()
-
-for i in range (110):
+ps.configuration.value
+for i in range (11):
     ps.configuration.recompute (i)
     print (ps.configuration.value [0])
 
@@ -20,16 +21,16 @@ ps.resetPath ()
 
 for i in range (111, 121):
     ps.configuration.recompute (i)
-    print (ps.configuration.value [0])
+    #print (ps.configuration.value [0])
 
 ps.addWaypoint (tuple (q_init))
 
 for i in range (121, 131):
     ps.configuration.recompute (i)
-    print (ps.configuration.value [0])
+    #print (ps.configuration.value [0])
 
 ps.start ()
 
 for i in range (131, 240):
     ps.configuration.recompute (i)
-    print (ps.configuration.value [0])
+    #print (ps.configuration.value [0])
