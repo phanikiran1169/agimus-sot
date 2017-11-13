6681a0b38b41c3d7ae0cccc83e9f52618b868799
Joseph Mirabel <jmirabel@laas.fr>
2017-11-13 11:44:16 +0100

    Make RosInterface compatible with RunCommand

--- sot_hpp/ros_interface.py
+++ sot_hpp/ros_interface.py
@@ -1,34 +1,41 @@
 import rospy
 from std_srvs.srv import Trigger, TriggerResponse
 from hpp_ros_interface.srv import PlugSot, PlugSotResponse, SetString, SetJointNames
+from dynamic_graph_bridge_msgs.srv import RunCommand
 
 class RosInterface(object):
-    def __init__ (self, supervisor):
-        self.supervisor = supervisor
+    def __init__ (self, supervisor = None):
         rospy.Service('/sot/plug_sot', PlugSot, self.plugSot)
         rospy.Service('/sot/request_hpp_topics', Trigger, self.requestHppTopics)
+        self.runCommand = rospy.ServiceProxy ('/run_command', RunCommand)
+        self.supervisor = supervisor
 
     def plugSot (self, req):
         rsp = PlugSotResponse()
-        try:
-            status = self.supervisor.isSotConsistentWithCurrent (req.transition_id)
-            if not status:
-                rsp.msg = "Sot %d not consistent with sot %d" % (self.supervisor.currentSot, req.transition_id)
-        except Exception, e:
-            rospy.logerr(str(e))
-            rsp.success = False
-            rsp.msg = str(e)
-            return rsp
-        try:
-            self.supervisor.plugSot(req.transition_id)
-            rsp.success = True
-        except Exception, e:
-            msg = str(e)
-            rsp.success = False
+        if self.supervisor is not None:
+            try:
+                self.supervisor.plugSot(req.transition_id, True)
+            except Exception as e:
+                rsp.success = False
+                rsp.msg = str(e)
+                return rsp
+        else:
+            answer = self.runCommand ("supervisor.plugSot({}, True)".format(req.transition_id))
+            if len(answer.standarderror) != 0:
+                rospy.logerr(answer.standarderror)
+                rsp.success = False
+                rsp.msg = answer.standarderror
+                return rsp
+        rsp.success = True
         return rsp
 
-    def setupHppJoints(self, prefix = None):
-        names = [ prefix + n for n in self.supervisor.sotrobot.dynamic.model.names[2:] ]
+    def setupHppJoints(self, prefix = ""):
+        if self.supervisor is not None:
+            names = self.supervisor.getJointList(prefix = prefix)
+        else:
+            answer = self.runCommand ("supervisor.getJointList(prefix = '{}')".format(prefix))
+            print answer
+            exec ("names = " + answer.result)
         setJoints = rospy.ServiceProxy ('/hpp/target/set_joint_names', SetJointNames)
         ans = setJoints (names)
         if not ans.success:
@@ -41,7 +48,14 @@ class RosInterface(object):
                 'hppjoint': rospy.ServiceProxy ('/hpp/target/add_operational_frame', SetString),
                 'vel_hppjoint': rospy.ServiceProxy ('/hpp/target/add_operational_frame_velocity', SetString),
                 }
-        topics = self.supervisor.topics()
+        if self.supervisor is not None:
+            topics = self.supervisor.topics()
+        else:
+            cmd = "{ n: { k: v for k, v in t.items() if k != 'signalGetters' } for n, t in supervisor.topics().items() }"
+            answer = self.runCommand (cmd)
+            print cmd
+            print answer
+            exec ("topics = " + answer.result)
         for n, t in topics.items():
             for k in ['hppjoint', 'hppcom']:
                 if t.has_key(k):
