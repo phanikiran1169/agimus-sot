7ed622c7ff2de900027083ec2aa0ec48f3d3b2a5
Joseph Mirabel <jmirabel@laas.fr>
2017-11-28 19:18:07 +0100

    Update Posture task gain

--- sot_hpp/tools.py
+++ sot_hpp/tools.py
@@ -75,12 +75,7 @@ class Posture(Manifold):
         self.tp.setWithDerivative (True)
 
         # Set the gain of the posture task
-        setGain(self.tp.gain,0)
-        # setGain(self.tp.gain,1)
-        # setGain(self.tp.gain,10)
-        # setGain(self.tp.gain,200)
-        # setGain(self.tp.gain,(4.9,0.9,0.01,0.9))
-        # setGain(self.tp.gain,(9.8,1.8,0.02,1.8))
+        setGain(self.tp.gain,10)
         plug(self.tp.gain.gain, self.tp.controlGain)
         plug(self.tp.error, self.tp.gain.error)
         self.tasks = [ self.tp ]
