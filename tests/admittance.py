2fd3a298b4282fb45952c0fb0db55d7c0e87769d
Joseph Mirabel <jmirabel@laas.fr>
2018-06-15 15:26:14 +0200

    Update tests/admittance.py

--- tests/admittance.py
+++ tests/admittance.py
@@ -46,48 +46,14 @@ N = int(1.5 / dt)
 # N = 30
 # }}}
 
-### Measurements (Feedback)
+### Admittance controller
 # {{{
 
-## omega -> theta
-omega2theta = IntegratorEulerVectorMatrix ("omega2theta")
-omega2theta.pushDenomCoef(((0,),))
-omega2theta.pushDenomCoef(((1,),))
-omega2theta.pushNumCoef(((1,),))
-
-omega2theta.sin.value = (0,)
-omega2theta.initialize()
-omega2theta.setSamplingPeriod(dt)
-
-## theta -> phi = theta - theta0
-theta2phi = dgsc.Add_of_vector("theta2phi")
-theta2phi.setCoeff1 ( 1)
-theta2phi.setCoeff2 (-1)
-theta2phi.sin2.value = (theta0,)
-# theta2phi.sin1 <- theta
-
-## phi -> torque
-phi2torque = IntegratorEulerVectorMatrix ("phi2torque")
-phi2torque.pushDenomCoef(((1,),))
-phi2torque.pushNumCoef(((k,),))
-phi2torque.pushNumCoef(((d,),))
-if M != 0: phi2torque.pushNumCoef(((M,),))
-
-phi2torque.sin.value = (0,)
-phi2torque.initialize()
-phi2torque.setSamplingPeriod(dt)
-
-## Plug signals
-plug (omega2theta.sout, theta2phi.sin1)
-plug (theta2phi.sout  , phi2torque.sin)
-# }}}
-
-### Feed-forward - non-contact phase
-# {{{
+from sot_hpp.control.gripper import AdmittanceControl
 
 # z  = 0
 # z  = sqrt(2)/2
-z  = 1
+z  = 1.
 # z  = 5
 # wn = 0.1
 # wn = 1.
@@ -101,15 +67,6 @@ print "Non-contact phase - PID:"
 print " - z  =", z
 print " - wn =", wn
 
-pos_control = controllers.secondOrderClosedLoop ("position_controller", wn, z, dt, (0,))
-pos_control.reference.value = (est_theta0,)
-
-# }}}
-
-### Feed-forward - contact phase
-# {{{
-
-# dY/dt + Y = X
 # tau = 0.1
 tau = 1.
 # tau = 10.
@@ -117,6 +74,7 @@ tau = 1.
 # alpha_m, alpha_M = root_second_order(4*z**2 * est_M * k - d**2, 4*z**2*tau*k - 2*d, 1)
 # alpha = alpha_m if alpha_m > 0 else alpha_M
 # alpha = min (max(0.5, alpha), 1.5)
+# alpha = 0.1
 alpha = 1.
 # alpha = 10.
 
@@ -125,15 +83,21 @@ print " - tau   =", tau
 # print " - alpha_m =", alpha_m
 # print " - alpha_M =", alpha_M
 print " - alpha =", alpha
-denom = alpha * M + tau
-print " - beta  1 =", alpha / denom
-print " - alpha 0 =", alpha * k / denom
-print " - alpha 1 =", (1+alpha*d)/denom
-print " - alpha 2 =", 1
+# denom = alpha * M + tau
+# print " - beta  1 =", alpha / denom
+# print " - alpha 0 =", alpha * k / denom
+# print " - alpha 1 =", (1+alpha*d)/denom
+# print " - alpha 2 =", 1
 
-torque_controller = controllers.Controller ("torque_controller", (alpha,), (1., tau), dt, (0,))
-torque_controller.addFeedback()
+admittance_controller = AdmittanceControl ("admittance",
+        (est_theta0,), (0.,), dt, (1e-5,),
+        wn = wn, z = z, alpha = alpha, tau = tau,)
 
+# }}}
+
+### Measurements (Feedback)
+# {{{
+admittance_controller.setupFeedbackSimulation(M, d, k, (theta0,))
 # }}}
 
 ### Input
@@ -153,19 +117,6 @@ input = constant
 
 ### Stability study
 # {{{
-
-# }}}
-
-### Setup switch between the two control scheme
-# {{{
-
-from sot_hpp.control.switch import ControllerSwitch
-
-switch = ControllerSwitch ("controller_switch",
-        (pos_control.outputDerivative, torque_controller.output),
-        (1e-5,))
-
-plug(switch.signalOut, omega2theta.sin)
 
 # }}}
 
@@ -190,42 +141,39 @@ for i in range(1,N):
     if phi < 0:
         if torque_control:
             print "Loosing contact at time", t
-            # measured_torque = (1e-4,)
-        # else:
         measured_torque = (0,)
     else:
-        measured_torque = phi2torque.sout.value
+        measured_torque = admittance_controller.phi2torque.output.value
         if len(measured_torque) != 1:
             print "Assuming very small measured torque at", t, phi
             measured_torque = (1e-4,)
 
-    if pos_control.hasFeedback:
-        pos_control.measurement.value = (theta,)
+    admittance_controller.referenceTorqueIn.value = input_torque
 
-    torque_controller.reference  .value = input_torque
-    torque_controller.measurement.value = measured_torque
-    switch.measurement.value = measured_torque
+    admittance_controller.switchEventCheck.recompute(t)
+    admittance_controller.output.recompute(t);
 
-    switch.event.check.recompute(t)
-    phi2torque.sout.recompute(t);
+    # TODO: I do not know whether this should be done or not.
+    #       It lack a model of interaction when the contact is created...
+    admittance_controller.phi2torque.output.recompute(t);
 
-    if not torque_control and switch.latch.out.value == 1:
+    if not torque_control and admittance_controller.switch.latch.out.value == 1:
         # Switch to torque control
         print "Switch to torque control at", t
         torque_control = True
-    elif torque_control and switch.latch.out.value == 0:
+    elif torque_control and admittance_controller.switch.latch.out.value == 0:
         print "Switch to position control at", t
         torque_control = False
 
     ts     .append(t * dt)
-    conditions.append(switch.latch.out.value)
+    conditions.append(admittance_controller.switch.latch.out.value)
     inputs .append(input_torque)
-    omegas .append(omega2theta.sin.value[0])
-    thetas .append(omega2theta.sout.value[0])
+    omegas .append(admittance_controller.omega2theta.reference.value[0])
+    thetas .append(admittance_controller.omega2theta.output   .value[0])
     torques.append(measured_torque)
 
-    theta = omega2theta.sout.value[0]
-    phi = theta2phi.sout.value[0]
+    theta = admittance_controller.omega2theta.output.value[0]
+    phi = admittance_controller.theta2phi.sout.value[0]
     t += 1
 
 # }}}
