6e1f18149f62f00cb9b97df86421d0f5ca46cb1b
Joseph Mirabel <jmirabel@laas.fr>
2020-05-18 21:58:45 +0200

    [Minor] Fix header inclusion

--- src/holonomic-constraint.cc
+++ src/holonomic-constraint.cc
@@ -24,7 +24,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <../src/holonomic-constraint.hh>
+#include "holonomic-constraint.hh"
 
 #include <dynamic-graph/command-bind.h>
 #include <dynamic-graph/factory.h>
