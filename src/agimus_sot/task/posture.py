78d1820984c55d36204693044b1d02b9c8f6cf63
Florent Lamiraux <florent.lamiraux@laas.fr>
2022-06-10 12:10:59 +0200

    [Posture] Initialize posture task with configuration of robot
      The robot configuration was read in the dynamic entity but until the
      graph is computed the first time the value of the signal does not
      correspond to the robot configuration.

--- src/agimus_sot/task/posture.py
+++ src/agimus_sot/task/posture.py
@@ -44,7 +44,7 @@ class Posture(Task):
         self._task.dyn = sotrobot.dynamic
         self._feature = FeaturePosture(n + '_feature_')
 
-        q = sotrobot.dynamic.position.value
+        q = sotrobot.device.state.value
         self._feature.state.value = q
         self._feature.posture.value = q
 
